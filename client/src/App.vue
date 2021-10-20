06f2ed544b985f9dcf1161ef7eda46231a2758ee
haroonym <younas.h03@htlwienwest.at>
2021-10-20 20:56:33 +0200

    Fertig!

--- client/src/App.vue
+++ client/src/App.vue
@@ -3,13 +3,16 @@
     <v-main>
       <v-app-bar class="blue grey lighten-5" app>
         <div class="d-flex align-center">
-          <img class="mt-10" src="logo.png" width="100" />
+          <img style="margin-top: 75px" src="logo.png" width="100" />
           <h1>Movies</h1>
         </div>
+
         <v-spacer></v-spacer>
+
         <v-btn href="https://www.imdb.com/movies-in-theaters/" target="_blank" text
           >Lastest Realese <v-icon>mdi-open-in-new</v-icon>
         </v-btn>
+
       </v-app-bar>
       <router-view />
     </v-main>
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,5 +1,17 @@
 <template>
-  <p>{{ movies }}</p>
+  <div>
+    <v-alert class="mx-auto" color="blue-grey darken-2 white--text" dark width="75%"> <b>Rent selected movies:</b> {{ filteredItem }} </v-alert>
+    <v-data-table
+      :headers="headers"
+      :items="movies"
+      :items-per-page="6"
+      :footer-props="{ itemsPerPageOptions: [6, 12, 18] }"
+      show-select
+      toggles-select-all
+      v-model="selectedItem"
+    >
+    </v-data-table>
+  </div>
 </template>
 
 <script>
@@ -7,12 +19,30 @@ import axios from 'axios';
 export default {
   name: 'Home',
   data() {
-    return { movies: [] };
+    return {
+      movies: [],
+      selectedItem: [],
+      headers: [
+        { text: 'Title', value: 'title', class: 'blue-grey lighten-5' },
+        { text: 'Director', value: 'director', class: 'blue-grey lighten-5' },
+        { text: 'Genre', value: 'main_genre', class: 'blue-grey lighten-5' },
+        { text: 'Year', value: 'year', width: 100, class: 'blue-grey lighten-5' },
+        { text: 'Rated', value: 'rated', class: 'blue-grey lighten-5' },
+        { text: 'Plot ', value: 'plot' },
+      ],
+    };
   },
   created() {
     this.getMovies();
   },
-  components: {},
+  computed: {
+    filteredItem: function() {
+      return this.selectedItem
+        .filter((el) => el.title)
+        .map(({ title }) => title)
+        .join(' | ');
+    },
+  },
   methods: {
     async getMovies() {
       try {
