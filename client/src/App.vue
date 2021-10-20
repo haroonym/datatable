48155c3ebc0f1262c5bf5c2f138b42148904c7c5
haroonym <younas.h03@htlwienwest.at>
2021-10-20 19:41:36 +0200

    Daten aufgerufen, Navbar erstellt, Website Title geändert, Icon geändert

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,50 +1,22 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
     <v-main>
-      <router-view/>
+      <v-app-bar class="blue grey lighten-5" app>
+        <div class="d-flex align-center">
+          <img class="mt-10" src="logo.png" width="100" />
+          <h1>Movies</h1>
+        </div>
+        <v-spacer></v-spacer>
+        <v-btn href="https://www.imdb.com/movies-in-theaters/" target="_blank" text
+          >Lastest Realese <v-icon>mdi-open-in-new</v-icon>
+        </v-btn>
+      </v-app-bar>
+      <router-view />
     </v-main>
   </v-app>
 </template>
 
 <script>
-
 export default {
   name: 'App',
 
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,15 +1,30 @@
 <template>
-  <hello-world />
+  <p>{{ movies }}</p>
 </template>
 
 <script>
-  import HelloWorld from '../components/HelloWorld'
-
-  export default {
-    name: 'Home',
-
-    components: {
-      HelloWorld,
+import axios from 'axios';
+export default {
+  name: 'Home',
+  data() {
+    return { movies: [] };
+  },
+  created() {
+    this.getMovies();
+  },
+  components: {},
+  methods: {
+    async getMovies() {
+      try {
+        const { data } = await axios({
+          url: 'http://127.0.0.1:3000/movies',
+          method: 'GET',
+        });
+        this.movies = data;
+      } catch (error) {
+        console.error(error);
+      }
     },
-  }
+  },
+};
 </script>
